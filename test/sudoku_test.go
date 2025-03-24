1d2d5a1da75d2e9b033beed1443761c7d7c3ece9
tonimatasdev <me@tonimatas.dev>
2025-03-24 20:07:57 +0100

    Do more sudokus on test

--- test/sudoku_test.go
+++ test/sudoku_test.go
@@ -7,8 +7,16 @@ import (
 
 func TestSudoku(t *testing.T) {
 	sudoku := internal.Sudoku("906000000,020005070,000002400,000039000,307000010,000000006,700800205,405600030,100300000")
+	sudoku1 := internal.Sudoku("000803000,500000003,040000020,006000500,080105070,007000600,030000080,400000007,000209000")
+	sudoku2 := internal.Sudoku("800000000,003600000,070090200,050007000,000045700,000100030,001000068,008500010,090000400")
+	sudoku3 := internal.Sudoku("020060000,000007009,600400800,100000000,000030000,000000007,003008002,700200000,000010080")
+	sudoku4 := internal.Sudoku("000000907,000420180,000705026,100904000,050000040,000507009,920108000,034059000,507000000")
 
-	if !internal.VerifySudoku(sudoku) {
+	if !internal.VerifySudoku(sudoku) &&
+		!internal.VerifySudoku(sudoku1) &&
+		!internal.VerifySudoku(sudoku2) &&
+		!internal.VerifySudoku(sudoku3) &&
+		!internal.VerifySudoku(sudoku4) {
 		t.Error("Sudoku is wrong")
 	}
 }
